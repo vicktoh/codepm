492e3b4be5d20fe28bb3e7cdcce5d5d198682a38
vicktoh <vadelowo@yahoo.com>
2022-04-20 21:28:18 +0100

    preview landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Heading, Text } from '@chakra-ui/react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex width="100vw" height="100vh" alignItems="center" justifyContent="center">
+      <Heading>CODE's Project management tool</Heading>
+      <Text>This is an inital setup of for continous integration and continous deployment</Text>
+    </Flex>
+      
+      
+    
   );
 }
 
